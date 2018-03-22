fe6dda649a8b5366e1e77a38b932df6adffb3016
Lucía de la Granda Prieto <luciadelagranda@gmail.com>
2018-03-22 22:13:15 +0100

    Juntando dbmanager

--- src/main/java/asw/Incidence.java
+++ src/main/java/asw/Incidence.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 public class Incidence {
 	
 	@Id
-  private ObjectId _id;
+	private ObjectId _id;
 
 	private String username;
 	private String password;
