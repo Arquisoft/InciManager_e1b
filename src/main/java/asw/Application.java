0c0d0eb9824db6d3a04bc5192f24b0cfe6f3cd68
Sara Grimaldos <UO251782@uniovi.es>
2018-03-22 21:25:35 +0100

    Plantilla proyecto actualizada

---
+++ src/main/java/asw/Application.java
@@ -0,0 +1,17 @@
+package asw;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
