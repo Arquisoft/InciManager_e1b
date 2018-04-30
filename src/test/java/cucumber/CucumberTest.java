361790cbae24088f997fc8403966c3f1478e0559
mistermboy <pabloyo97@hotmail.com>
2018-04-30 20:18:28 +0200

    esqueleto new cucumber

---
+++ src/test/java/cucumber/CucumberTest.java
@@ -0,0 +1,11 @@
+package cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features")
+public class CucumberTest{
+}
