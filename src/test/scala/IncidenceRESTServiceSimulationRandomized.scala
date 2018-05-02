7fc314bfa2145a5fbdfb96e925ce755af9b1021a
Esteban Montes <sankoxh@live.com>
2018-05-02 22:57:12 +0200

    Añadidas dependencias de gatling y los plugins necesarios (compilador scala y gatling-plugin), ahora ya se pueden hacer las pruebas de carga usando maven, sin necesidad de descargar gatling

--- src/test/scala/IncidenceRESTServiceSimulationRandomized.scala
+++ src/test/scala/IncidenceRESTServiceSimulationRandomized.scala
@@ -7,11 +7,11 @@ import io.gatling.jdbc.Predef._
 
 /**
 	Tutorial probando REST API: https://www.blazemeter.com/blog/api-load-testing-with-gatling
-	Se crean 1000 incidencias a lo largo de 35 segundos, en menos segundos empieza a haber problemas, 10k a lo largo de 350s
+	Se crean de 1000 a 2000 incidencias a lo largo de 35 segundos
 	Las incidencias seran creadas en intervalos random de tiempo
 */
 
-class IncidenceRESTServiceSimulation extends Simulation {
+class IncidenceRESTServiceSimulationRandomized extends Simulation {
 
 	val httpProtocol = http
 		.baseURL("http://localhost:8090")
@@ -20,11 +20,11 @@ class IncidenceRESTServiceSimulation extends Simulation {
 		.contentTypeHeader("application/json")
 		.userAgentHeader("curl/7.54.0")
 
-	val scn = scenario("IncidenceRESTServiceSimulation")
+	val scn = scenario("IncidenceRESTServiceSimulationRandomized")
 		.exec(http("request_0")
 			.post("/incidence-creator")
 			.body(StringBody(""" {"ident":"entidad2","password":"123456","kind":2,"name":"inc_GHKB","description":"Nueva lectura de humedad","location":"2919,178","tags":["Nieve","Fuego","Niebla","Terremoto"],"additionalInformation":"http://puntoverdeleon.com.mx/wp-content/uploads/2016/09/imagen-de-prueba-320x240.jpg","properties":{"p0":"v0","p1":"v1","p2":"v2","p3":"v3"},"state":"Abierta","notification":"si","expireAt":"2018-10-25 10:02:29.769579","assignedTo":"oper_rUxl"} """)).asJSON
 		)
 
-	setUp(scn.inject(rampUsers(1000) over(35 seconds) randomized)).protocols(httpProtocol)
+	setUp(scn.inject(rampUsersPerSec(1000) to (2000) during(35) randomized)).protocols(httpProtocol)
 }
